a2abfa55c0a39f8d1ee46d9afc469a27d74a19d3
5adhu <xyablukox1@gmail.com>
2020-10-08 20:34:00 +0300

    Practice #3

---
+++ 3_1.py
@@ -0,0 +1,21 @@
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+Name = input('Enter your name')
+Surname = input('Enter your surname')
+Phone = input('Enter your phone')
+Street = input('Enter your street')
+House = input('Enter your house number')
+Apnum = input('Enter your apartment number')
+City = input('Enter your city')
+Index = input('Enter your index')
+Сon = input('Enter your country')
